9dee50e05f7cf9371ac496d6b529c73207866361
Jaime-Cardos0 <jaimecardoso551@gmail.com>
2026-05-10 15:09:48 +0100

    fix: remove invalid outputFileTracingRoot configuration in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  outputFileTracingRoot: "C:/Users/Jaime Cardoso/package-lock.json"
 };
 
 export default nextConfig;

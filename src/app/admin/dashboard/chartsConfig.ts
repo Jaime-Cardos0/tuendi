31f3501cf21d10ac76f85b7f4eec3a4b5130f403
Jaime-Cardos0 <jaimecardoso551@gmail.com>
2026-05-12 22:06:24 +0100

    feat: update radialBarOptions to use string literals for theme text colors

--- src/app/admin/dashboard/chartsConfig.ts
+++ src/app/admin/dashboard/chartsConfig.ts
@@ -64,8 +64,8 @@ export const radialBarOptions: ApexOptions = {
         hollow: { size: "30%" },
         track: { background: "transparent" },
         dataLabels: {
-        name: { fontSize: "12px", color: theme.text.muted },
-        value: { fontSize: "14px", fontWeight: "normal", color: theme.text.primary },
+        name: { fontSize: "12px", color: "text.muted" },
+        value: { fontSize: "14px", fontWeight: "normal", color: "text.primary" },
         },
     },
     },
@@ -74,7 +74,7 @@ export const radialBarOptions: ApexOptions = {
     legend: {
     show: true,
     position: "bottom",
-    labels: { colors: theme.text.secondary, },
+    labels: { colors: "text.secondary", },
     width: 2,
     height: 2,
     floating: true,
